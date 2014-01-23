f7de30f815a39fe11a38a6fa23b11175e0de88ea
Chris Billington <chrisjbillington@gmail.com>
2014-01-23 14:04:07 +1100

    Restructured for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='spinapi',
+      version='1.0',
+      description='Python wrapper around the Spincore PulseBlaster API using ctypes.',
+      author='Chris Billington',
+      author_email='chrisjbillington@gmail.com',
+      url='https://bitbucket.org/cbillington/spinapi/',
+      license="BSD",
+      packages=['spinapi']
+     )
