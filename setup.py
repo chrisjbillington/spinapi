2607a69ecffc83d593c6cefb4e052119e41ac4fe
Chris Billington <chrisjbillington@gmail.com>
2014-01-23 15:00:38 +1100

    Added instructions to setup.py for uploading to PyPI

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# To upload a version to PyPI, run:
+#
+#    python setup.py sdist upload
+#
+# If the package is not registered with PyPI yet, do so with:
+#
+# python setup.py register
+
 from distutils.core import setup
 
 setup(name='spinapi',
