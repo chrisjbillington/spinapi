0c97688ee2d29574e7ad7ce05d59dde4f33ec0cd
Chris Billington <chrisjbillington@gmail.com>
2014-01-23 15:30:51 +1100

    Removed redundant PKG-INFO file. Added MANIFEST.in
    Added __version__ attribute to module.
    
    Bumped patch version number

--- setup.py
+++ setup.py
@@ -11,12 +11,11 @@
 from distutils.core import setup
 
 setup(name='spinapi',
-      version='1.0',
+      version='1.0.1',
       description='Python wrapper around the Spincore PulseBlaster API using ctypes.',
       author='Chris Billington',
       author_email='chrisjbillington@gmail.com',
       url='https://bitbucket.org/cbillington/spinapi/',
       license="BSD",
-      packages=['spinapi'],
-      package_data={'spinapi': ['LICENSE.txt', 'README.md']}
+      packages=['spinapi']
      )
--- spinapi/__init__.py
+++ spinapi/__init__.py
@@ -1,1 +1,3 @@
+import pkg_resources
+__version__ = pkg_resources.require("spinapi")[0].version
 from spinapi import *
