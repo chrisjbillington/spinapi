13f00d31d7a16f1571b531f016d31d6d81e2ccb1
chrisjbillington <chrisjbillington@gmail.com>
2020-05-30 01:27:55 -0400

    Bundle both shared objects so we can ship a noarch package for now.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 import platform
 
-VERSION = '3.2.1'
+VERSION = '3.2.2'
 
 # Auto generate a __version__ package for the package to import
 with open(os.path.join('spinapi', '__version__.py'), 'w') as f:
@@ -10,12 +10,7 @@ with open(os.path.join('spinapi', '__version__.py'), 'w') as f:
 
 arch = platform.architecture()
 
-if arch == ('32bit', 'ELF'):
-    bundled_shared_objects = ['libspinapi.so']
-elif arch == ('64bit', 'ELF'):
-    bundled_shared_objects = ['libspinapi64.so']
-else:
-    bundled_shared_objects = []
+bundled_shared_objects = ['libspinapi.so', 'libspinapi64.so']
     
 setup(name='spinapi',
       version=VERSION,
